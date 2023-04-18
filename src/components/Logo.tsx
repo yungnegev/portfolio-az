452601e030cf00a02f5836be0e3a07d468e2143f
Arthur <neiklotrr@gmail.com>
2023-04-18 20:03:20 +0300

    Navbar complete

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,16 +2,25 @@ import Link from 'next/link'
 import React from 'react'
 import { motion as m } from 'framer-motion'
 
+
+
 const Logo = () => {
+
+  const MLink = m(Link)
+
   return (
     <div className='flex justify-center items-center mt-2'>
-        <Link 
+        <MLink 
             href='/'
             className='flex justify-center items-center w-16 h-16 text-white bg-zinc-950 rounded-full
                       font-thin tracking-widest text-2xl'   
+            whileHover={{
+                scale: 1.05,
+                backgroundColor:["#09090b", "#5eead4","#fcd34d","#be274c", "#09090b"],
+            }}
             >
                 AZ
-        </Link>
+        </MLink>
     </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,49 @@
 import Link from 'next/link'
 import React from 'react'
 import Logo from './Logo'
+import { useRouter } from 'next/router'
+import { AiFillGithub } from 'react-icons/ai'
+
+type CustomLinkProps = {
+    href: string,
+    title: string,
+    className: string
+}
+
+const CustomLink = ({ href, title, className }: CustomLinkProps) => {
+
+    const path = useRouter().pathname
+
+    console.log(path)
+
+    return(
+        <Link href={href} className={`${className} relative group`}>
+            {title}
+
+            <span className={`bg-zinc-900 h-[1px] inline-block absolute left-0 -bottom-1
+                            group-hover:w-full transition-[width] ease duration-300 
+                            ${path === href ? 'w-full' : 'w-0'}`}>&nbsp;</span>
+        </Link>
+    )
+}
 
 const Navbar = () => {
   return (
     <header className='w-full px-32 py-8 font-bold flex items-center justify-between'>
         <nav>
-            <Link href={'/'}>Home</Link>
-            <Link href={'/about'}>About</Link>
-            <Link href={'/projects'}>Projects</Link>
+            <CustomLink href='/' title='Home' className='mr-4'/>
+            <CustomLink href='/about' title='About' className='mx-4'/>
+            <CustomLink href='/projects' title='Projects' className='ml-4'/>
         </nav>
-        <Logo />
+
+        <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+            <Logo />
+        </div>
+
         <nav>
-            <Link href={'/'} target='_blank'>sfsf</Link>
+            <Link href={'https://github.com/yungnegev'} target='_blank'>
+                <AiFillGithub size={28} className='hover:text-zinc-700 active:scale-90'/>
+            </Link>
         </nav>
     </header>
   )
