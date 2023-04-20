4f46141c590778576600993a4dad83ed7429e710
Arthur <neiklotrr@gmail.com>
2023-04-20 17:21:16 +0300

    Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='bt-1 flex items-center justify-center p-8 text-zinc-500 text-xs'>
+      © 2022 - All Rights Reserved.
+    </footer>
   )
 }
 
